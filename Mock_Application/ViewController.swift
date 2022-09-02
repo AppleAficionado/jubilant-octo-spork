2a407e949b08eca02bc0087b0b7ee8dc3b181971
Nathan_Visser <nathan.visser@capco.com>
2022-09-02 14:07:04 -0400

    Added Alamofire for networking
    Signed-off-by: Nathan_Visser <nathan.visser@capco.com>

--- Mock_Application/ViewController.swift
+++ Mock_Application/ViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 import SnapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        AF.request("https://pokeapi.co/api/v2/pokemon/charmander").response { response in
+            debugPrint(response)
+        }
         
         let box = UIView()
         box.backgroundColor = UIColor.black
@@ -23,7 +27,5 @@ class ViewController: UIViewController {
             make.edges.equalTo(self.view)
         }
     }
-
-
 }
 
